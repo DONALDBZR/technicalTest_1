b25ba32f92e0eae6350e61079699dca48cb71df7
DONALDBZR <andygaspard@hotmail.com>
2022-12-21 00:05:25 +0400

    UPDATE 0.1.6 Adding the controller to process the data

--- Routes.php
+++ Routes.php
@@ -13,5 +13,8 @@ switch ($_SERVER['REQUEST_METHOD']) {
         break;
     case 'POST':
         switch ($_SERVER['REQUEST_URI']) {
+            case '/Customers/Create':
+                $Router = new Router("GET", "/Customers/Create", "/Controllers/CustomersCreate.php");
+                break;
         }
 }
