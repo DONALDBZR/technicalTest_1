c932c73681b0ef3b54b91c98acf591e32969494d
DONALDBZR <andygaspard@hotmail.com>
2022-12-20 20:43:36 +0400

    UPDATE 0.1.1 View for the creation of customer

--- Routes.php
+++ Routes.php
@@ -6,6 +6,9 @@ switch ($_SERVER['REQUEST_METHOD']) {
             case '/':
                 $Router = new Router("GET", "/", "/Views/Homepage.php");
                 break;
+            case '/Customers/New':
+                $Router = new Router("GET", "/Customers/New", "/Views/CustomersNew.php");
+                break;
         }
         break;
     case 'POST':
