0c85e51a8c2c66ca7bb7186bc15276fbfa9e6db6
DONALDBZR <andygaspard@hotmail.com>
2022-12-20 18:36:50 +0400

    UPDATE 0.0.3 View for the homepage

--- Views/Homepage.php
+++ Views/Homepage.php
@@ -1,5 +1,3 @@
 <?php
-// Importing Routes
 require_once "{$_SERVER['DOCUMENT_ROOT']}/Routes.php";
-// Importing the required page
 require_once "{$_SERVER['DOCUMENT_ROOT']}/Public/Pages/Homepage.html";
