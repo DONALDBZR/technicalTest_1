7aa2bd4bdfe03b011cbacc41805370580af7f036
DONALDBZR <andygaspard@hotmail.com>
2022-12-23 19:54:38 +0400

    UPDATE 0.1.14 Allowing the mail client to sent a mail to the customer

--- Public/Scripts/JS/Homepage.js
+++ Public/Scripts/JS/Homepage.js
@@ -2,6 +2,33 @@
  * The Application that is going to be rendered in the DOM
  */
 class Application extends React.Component {
+    constructor(props) {
+        super(props);
+        /**
+         * States of the properties of the application
+         */
+        this.state = {
+            /**
+             * customers
+             * @type {array}
+             */
+            customers: [],
+        };
+    }
+    /**
+     * Retrieving the customers data set
+     */
+    getCustomers() {
+        fetch("/Customers",
+            {
+                method: "GET",
+            }
+        )
+            .then((response) => response.json())
+            .then((data) => this.setState({
+                customers: data,
+            }));
+    }
     /**
      * Renders the components that are being returned
      * @returns {Application} Component
@@ -22,6 +49,12 @@ class Header extends Application {
  * The component that is the main
  */
 class Main extends Application {
+    constructor(props) {
+        super(props);
+    }
+    componentDidMount() {
+        this.getCustomers();
+    }
     render() {
         return (
             <main>
@@ -41,6 +74,21 @@ class Main extends Application {
                         <div>Active</div>
                         <div>Date Created</div>
                     </div>
+                    {this.state.customers.map((customer) => {
+                        return (
+                            <div>
+                                <div>{customer.title}</div>
+                                <div>{customer.firstName}</div>
+                                <div>{customer.middleName}</div>
+                                <div>{customer.lastName}</div>
+                                <div>
+                                    <a href={`mailto:${customer.mailAddress}`}>{customer.mailAddress}</a>
+                                </div>
+                                <div>{customer.active}</div>
+                                <div>{customer.dateCreated}</div>
+                            </div>
+                        )
+                    })}
                 </div>
             </main>
         );
