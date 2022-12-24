6f3126e5151e02a42a91f02db1cc6f8f0fafab8c
DONALDBZR <andygaspard@hotmail.com>
2022-12-24 11:37:09 +0400

    UPDATE 0.1.22 Improving the Controller

--- Controllers/CustomersRead.php
+++ Controllers/CustomersRead.php
@@ -2,4 +2,10 @@
 require_once "{$_SERVER['DOCUMENT_ROOT']}/Routes.php";
 require_once "{$_SERVER['DOCUMENT_ROOT']}/Models/Customer.php";
 $Customer = new Customer();
-$Customer->read();
+if ($_SERVER["REQUEST_URI"] == "/Customers") {
+    $Customer->read();
+} else {
+    $request_uri = str_replace("/Customers/", "", $_SERVER['REQUEST_URI']);
+    $parameters = explode("/", $request_uri);
+    $Customer->sortRead($parameters[0], $parameters[1]);
+}
